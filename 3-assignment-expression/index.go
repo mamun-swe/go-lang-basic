5f3167f7c65830638e1b72799e1a75edfd01333b
Mamun-swe <mamun.swe.277@gmail.com>
2022-02-16 17:42:38 +0600

    Changes in assignment expression

--- 3-assignment-expression/index.go
+++ 3-assignment-expression/index.go
@@ -7,4 +7,13 @@ func main() {
 	number = number + 5
 
 	fmt.Println(number)
+
+	number = number - 3
+	fmt.Println(number)
+
+	number = number * 2
+	fmt.Println(number)
+
+		number = number / 3
+	fmt.Println(number)
 }
