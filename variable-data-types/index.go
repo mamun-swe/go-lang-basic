19a34aae9d46576f6eb144178e6f8095a51aff59
Mamun-swe <mamun.swe.277@gmail.com>
2022-02-16 16:18:27 +0600

    Data type & variable added

--- variable-data-types/index.go
+++ variable-data-types/index.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome")
+	var name string = "Go lang!"
+	var age uint64 = 0
+	var maritalStatus bool
+
+	age = 5
+	maritalStatus = false
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(maritalStatus)
 }
