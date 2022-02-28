4c30183f276c996f54f0cc032947cee64085ea2e
Mamun-swe <mamun.swe.277@gmail.com>
2022-02-28 13:35:08 +0600

    Changes directory names

---
+++ day-3-assignment-expression/index.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 5
+	number = number + 5
+
+	fmt.Println(number)
+
+	number = number - 3
+	fmt.Println(number)
+
+	number = number * 2
+	fmt.Println(number)
+
+		number = number / 3
+	fmt.Println(number)
+}
